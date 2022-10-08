a226e50be7ea8e20122d35b68a337706a0a98d6d
Leonel Paz Gerez <104148448+leopazgerez@users.noreply.github.com>
2022-10-08 01:31:52 -0300

    Filtro

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,48 @@
+import 'package:desafiogrupal2/src/ui/pagecomponent/filter_component.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_switch/flutter_switch.dart';
-
-
-class FilterItem{
-  String? text;
-  bool? isActive;
-  IconData? icon;
-  Color? backgroundColor;
-
-  FilterItem({required this.text,required this.icon,this.isActive=false,this.backgroundColor=Colors.cyan});
-
-  void changeState(){
-    isActive=!isActive!;
-  }
-
-}
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(child: Container(color: Colors.grey,)),
-          AlertDialogContenido(),
-          Expanded(child: Container(color: Colors.grey,)),
-        ],
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+//  Parametros a recibir de la clase filtros seria: tamaño del container, colores, los filtros en base
+// los datos que estan cargados una variable de tipo funcion para que pueda interactuar con los de mas widgets
+// Habra que ver si se ajusta de manera correcta un VoidCallBack para la interaccion o si se debe hacer otro tipo
 
-class AlertDialogContenido extends StatefulWidget {
-  const AlertDialogContenido({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
   @override
-  State<AlertDialogContenido> createState() => _AlertDialogContenidoState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _AlertDialogContenidoState extends State<AlertDialogContenido> {
-  bool selected=false;
-  bool _value=false;
+class _MyHomePageState extends State<MyHomePage> {
+  bool _toggled4 = false;
+  bool _toggled1 = false;
+  bool _toggled2 = false;
+  bool _toggled3 = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-      color: Colors.white,
-      child: ListView(
-        children: [
-          SwitchListTile(
-              value: _value,
-              onChanged: (value){})
-        ],
-      )
-
-
-      /*SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Item(selected),
-            const Divider(),
-            Item(selected),
-            const Divider(),
-            Item(selected),
-            const Divider(),
-            Item(selected),
-            const Divider(),
-            Item(selected),
-            const Divider(),
-            Item(selected),
-            const Divider(),
-            Item(selected),
-
-          ],
-        ),
-      ),*/
-    );
-  }
-  Widget Item(bool value){
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: const [
-              CircleAvatar(child: Icon(Icons.confirmation_num),),
-              SizedBox(width: 15,),
-              Text("Mantenimiento",style: TextStyle(fontSize: 20,color: Colors.black),)
-            ],
-          ),
-          FlutterSwitch(
-              activeColor: Colors.cyan,
-              value: value,
-              onToggle: (bool value){
-                setState(() {
-                  value=!value;
-                });
-              })
-        ],
-      ),
+    return const Scaffold(
+      body: FilterComponent(),
     );
   }
 }

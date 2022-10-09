2297e0c7d0651144c69c120f4d602e7ff09f50a6
Rodrigo Reynaga <ronico041012@gmail.com>
2022-10-09 00:24:24 -0300

    agregamos enum e id a la clase

--- lib/src/ui/pagecomponent/filter_component.dart
+++ lib/src/ui/pagecomponent/filter_component.dart
@@ -8,129 +8,62 @@ class FilterComponent extends StatefulWidget {
 }
 
 class _FilterComponentState extends State<FilterComponent> {
-  late Toggle toggle;
-  bool toggled4 = false;
-  bool toggled1 = false;
-  bool toggled2 = false;
-  bool toggled3 = false;
+
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    List<FilterItem> filtros = [
+      FilterItem(id:Task.mantenimiento ,text: 'mantenimiento', icon: Icons.ac_unit,backgroundColor: Colors.redAccent),
+      FilterItem(id:Task.confeccion, text: 'Confeccion', icon: Icons.construction_rounded, isActive: true),
+      FilterItem(id:Task.calidad, text: 'Calidad', icon: Icons.construction_rounded),
+      FilterItem(id:Task.jefe_linea, text: 'Jefe linea', icon: Icons.construction_rounded),
+      FilterItem(id:Task.jefe_taller, text: 'Jefe taller', icon: Icons.construction_rounded, isActive: true),
+      FilterItem(id:Task.capacitacion, text: 'Capacitacion', icon: Icons.construction_rounded),
+      FilterItem(id:Task.corte, text: 'Corte', icon: Icons.construction_rounded),
+    ];
+    return Column(
       children: [
         FloatingActionButton(
-              child: const Icon(Icons.add),
-              onPressed: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        icon: const SizedBox(
-                            height: 20,
-                            width: 20,
-                            child: Align(
-                                alignment: Alignment.topRight,
-                                child: CloseButton())),
-                        iconPadding: EdgeInsets.zero,
-                        buttonPadding: EdgeInsets.zero,
-                        scrollable: true,
-                        content: Column(
-                              children: [
-                                _header(),
-                                const Divider(
-                                  thickness: 2.5,
-                                ),
-                                SizedBox(
-                                    height: 450,
-                                    width: 300,
-                                    child: StatefulBuilder(
-                                      builder:(BuildContext context, StateSetter setState){
-                                      return _body(setState);
-                                    }
-                                )),
-                                const SizedBox(
-                                  height: 20,
-                                ),
-                                _button( ),
-
-                              ],
-                            ),
-                      );
-                    });
-              }
-              ),
+            child: const Icon(Icons.add),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      icon: const SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: Align(
+                              alignment: Alignment.topRight,
+                              child: CloseButton())),
+                      iconPadding: EdgeInsets.zero,
+                      buttonPadding: EdgeInsets.zero,
+                      scrollable: true,
+                      content: Column(
+                        children: [
+                          _header(),
+                          const Divider(thickness: 2.5,),
+                          SizedBox(
+                              height: 450,
+                              width: 300,
+                              child: StatefulBuilder(builder:
+                                  (BuildContext context, StateSetter setState) {
+                                return _body(filtros,setState);
+                              })),
+                          const SizedBox(height: 20,),
+                          _button(),
+                        ],
+                      ),
+                    );
+                  });
+            }),
       ],
     );
   }
 
-  /*Center _bodyTest() {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          AlertDialog(
-              content: Container(
-                height: 150,
-                width: 75,
-                child: SingleChildScrollView(
-                  child: Column(children: <Widget>[
-                    Text('data'),
-                    Divider(),
-                    SwitchListTile(
-                      contentPadding: const EdgeInsets.all(3),
-                      value: _toggled1,
-                      onChanged: (bool value) {
-                        setState(() {
-                          _toggled1 = value;
-                        });
-                      },
-                      title: const Text('Toggle'),
-                      secondary: const Icon(Icons.ac_unit),
-                    ),
-                    const Divider(),
-                    SwitchListTile(
-                      contentPadding: const EdgeInsets.all(3),
-                      value: _toggled2,
-                      onChanged: (bool value) {
-                        setState(() {
-                          _toggled2 = value;
-                        });
-                      },
-                      title: const Text('Toggle 1'),
-                      secondary: const Icon(Icons.add_alert_outlined),
-                    ),
-                    const Divider(),
-                    SwitchListTile(
-                      value: _toggled3,
-                      onChanged: (bool value) {
-                        setState(() {
-                          _toggled3 = value;
-                        });
-                      },
-                      title: const Text('Toggle 2'),
-                      secondary: const Icon(Icons.adb),
-                    ),
-                    const Divider(),
-                    SwitchListTile(
-                      value: _toggled4,
-                      onChanged: (bool ola) {
-                        setState(() {
-                          _toggled4 = ola;
-                        });
-                      },
-                      title: const Text('Toggle 3'),
-                      secondary: const Icon(Icons.add_chart),
-                    )
-                  ]),
-                ),
-              ),
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15.0))))
-        ],
-      ),
-    );
-  }*/
 
-  Widget _header() {
+
+  _header() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: const [
@@ -142,61 +75,28 @@ class _FilterComponentState extends State<FilterComponent> {
     );
   }
 
-   _body(StateSetter setState) {
-    return Column(children: <Widget>[
-      SwitchListTile(
-        contentPadding: const EdgeInsets.all(3),
-        value: toggled1,
-        onChanged: (bool value) {
-          setState(() {
-            toggled1 = value;
-          });
-        },
-        title: const Text('Toggle'),
-        secondary: const Icon(Icons.ac_unit),
-      ),
-      const Divider(),
-      SwitchListTile(
-        contentPadding: const EdgeInsets.all(3),
-        value: toggled2,
-        onChanged: (bool value) {
-          setState(() {
-            toggled2 = value;
-          });
-        },
-        title: const Text('Toggle 1'),
-        secondary: const Icon(Icons.add_alert_outlined),
-      ),
-      const Divider(),
-      SwitchListTile(
-        contentPadding: const EdgeInsets.all(3),
-        value: toggled3,
-        onChanged: (bool value) {
-          setState(() {
-            toggled3 = value;
-          });
-        },
-        title: const Text('Toggle 2'),
-        secondary: const Icon(Icons.adb),
-      ),
-      const Divider(),
-      SwitchListTile(
-        contentPadding: const EdgeInsets.all(3),
-        value: toggled4,
-        onChanged: (bool value) {
-          setState(() {
-            print('cambiando estado');
-            toggled4 = value;
-          });
-        },
-        title: const Text('Toggle 3'),
-        secondary: const Icon(Icons.add_chart),
-      ),
-
-    ]);
+  _cell(FilterItem item,StateSetter setState) {
+    return SwitchListTile(
+      contentPadding: const EdgeInsets.all(3),
+      value: item.isActive,
+      onChanged: (bool value) {
+        setState(() {
+          item.onChange();
+        });
+      },
+      title: Text(item.text),
+      secondary: Icon(item.icon, color: item.backgroundColor,),
+    );
   }
 
-  Widget _button() {
+  _body(List<FilterItem> lista,StateSetter setState) {
+    return  ListView.separated(
+        itemBuilder: (context, index) => _cell( lista[index],setState),
+        separatorBuilder: (context, index) => const Divider(height: 2,),
+        itemCount:lista.length );
+  }
+
+  _button() {
     return Container(
         // color: Colors.blue,
         width: 300,
@@ -211,4 +111,34 @@ class _FilterComponentState extends State<FilterComponent> {
   }
 }
 
-enum Toggle { one, two, three, four }
+class FilterItem {
+  Task id;
+  String text;
+  bool isActive;
+  IconData icon;
+  Color backgroundColor;
+
+
+  FilterItem({
+      required this.id,
+      required this.text,
+      required this.icon,
+      this.isActive = false,
+      this.backgroundColor = Colors.lightGreenAccent});
+
+  void onChange() {
+    isActive = !isActive;
+  }
+
+}
+
+enum Task {
+  mantenimiento,
+  corte,
+  calidad,
+  jefe_taller,
+  limpieza,
+  capacitacion,
+  confeccion,
+  jefe_linea
+}

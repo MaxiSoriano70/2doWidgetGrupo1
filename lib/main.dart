2806d8aa637b55d4af8f8f3b274a125783c4a739
Leonel Paz Gerez <104148448+leopazgerez@users.noreply.github.com>
2022-10-10 18:53:28 -0300

    Update FilterComponent V2

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        scrollbarTheme: ScrollbarThemeData(
-          thumbColor: MaterialStatePropertyAll(Colors.teal),
-        )
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -37,7 +34,121 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  List<FilterItem> filters = [
+    FilterItem(
+        id: Task.mantenimiento,
+        text: 'Mantenimiento',
+        icon: Icons.settings,
+        backgroundColor: Colors.grey),
+    FilterItem(
+      id: Task.confeccion,
+      text: 'Corte',
+      icon: Icons.content_cut_sharp,
+      backgroundColor: Colors.pink.shade200,
+    ),
+    FilterItem(
+        id: Task.jefe_linea,
+        text: 'Calidad',
+        backgroundColor: Colors.purple.shade300,
+        icon: Icons.assignment_ind_outlined),
+    FilterItem(
+      id: Task.jefe_taller,
+      text: 'Jefe taller',
+      icon: Icons.groups_outlined,
+      backgroundColor: Colors.deepPurpleAccent,
+    ),
+    FilterItem(
+        id: Task.capacitacion,
+        text: 'Limpieza',
+        backgroundColor: Colors.blue,
+        icon: Icons.sanitizer_sharp),
+    FilterItem(
+        id: Task.corte,
+        text: 'Capacitación',
+        backgroundColor: Colors.green.shade700,
+        icon: Icons.school_sharp),
+    FilterItem(
+        id: Task.corte,
+        text: 'Confección',
+        backgroundColor: Colors.deepOrange,
+        icon: Icons.person),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+    FilterItem(
+        id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
+  ];
   @override
   Widget build(BuildContext context) {
     return
@@ -47,7 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
           context: context,
           builder: (BuildContext context) {
             return
-              FilterComponent(switchColor: Colors.teal.shade400);
+              FilterComponent(switchColor: Colors.teal.shade400, items: filters,);
           },
         );
       }),
--- lib/src/ui/pagecomponent/filter_component.dart
+++ lib/src/ui/pagecomponent/filter_component.dart
@@ -4,7 +4,9 @@ import 'package:flutter/material.dart';
 
 class FilterComponent extends StatefulWidget {
   final Color switchColor;
-  const FilterComponent({Key? key, this.switchColor = Colors.cyan})
+  final List<FilterItem>? items;
+  final VoidCallback? onTap;
+  const FilterComponent({Key? key, this.switchColor = Colors.cyan, this.items, this.onTap})
       : super(key: key);
 
   @override
@@ -12,54 +14,12 @@ class FilterComponent extends StatefulWidget {
 }
 
 class _FilterComponentState extends State<FilterComponent> {
+  ScrollController scrollPosicioned = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    List<FilterItem> filters = [
-      FilterItem(
-          id: Task.mantenimiento,
-          text: 'Mantenimiento',
-          icon: Icons.settings,
-          backgroundColor: Colors.grey),
-      FilterItem(
-        id: Task.confeccion,
-        text: 'Corte',
-        icon: Icons.content_cut_sharp,
-        backgroundColor: Colors.pink.shade200,
-      ),
-      FilterItem(
-          id: Task.jefe_linea,
-          text: 'Calidad',
-          backgroundColor: Colors.purple.shade300,
-          icon: Icons.assignment_ind_outlined),
-      FilterItem(
-        id: Task.jefe_taller,
-        text: 'Jefe taller',
-        icon: Icons.groups_outlined,
-        backgroundColor: Colors.deepPurpleAccent,
-      ),
-      FilterItem(
-          id: Task.capacitacion,
-          text: 'Limpieza',
-          backgroundColor: Colors.blue,
-          icon: Icons.sanitizer_sharp),
-      FilterItem(
-          id: Task.corte,
-          text: 'Capacitación',
-          backgroundColor: Colors.green.shade700,
-          icon: Icons.school_sharp),
-      FilterItem(
-          id: Task.corte,
-          text: 'Confección',
-          backgroundColor: Colors.deepOrange,
-          icon: Icons.person),
-      FilterItem(
-          id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
-      FilterItem(
-          id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
-      FilterItem(
-          id: Task.corte, text: 'Jefe línea', icon: Icons.construction_rounded),
-    ];
-    return _alertDialogCustom(filters);
+
+    return _alertDialogCustom(widget.items!);
   }
 
   _alertDialogCustom(List<FilterItem> filters) {
@@ -73,34 +33,32 @@ class _FilterComponentState extends State<FilterComponent> {
       content: Column(
         children: [
           Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                      height: 450,
-                      width: 300,
-                      child: StatefulBuilder(
-                          builder: (BuildContext context, StateSetter setState) {
-                        return _listFilterTwo(filters, setState);
-                      })),
-                ],
-              ),
+            child: Column(
+              children: [
+                SizedBox(
+                    height: 450,
+                    width: 300,
+                    child: StatefulBuilder(
+                        builder: (BuildContext context, StateSetter setState) {
+                      return _listFilterTwo(filters, setState);
+                    })),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 25,
-          ),
-          CustomButton(
-            backgroundColor: widget.switchColor,
-            text: 'Aceptar',
-            padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
+
         ],
       ),
       contentPadding: const EdgeInsets.all(15),
+      actionsAlignment: MainAxisAlignment.center,
+      actionsPadding: const EdgeInsets.only(bottom: 25),
+      actions: [
+        CustomButton(
+          backgroundColor: widget.switchColor,
+          text: 'Aceptar',
+          padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+          onTap: widget.onTap,
+        ),
+      ],
     );
   }
 
@@ -172,9 +130,20 @@ class _FilterComponentState extends State<FilterComponent> {
   }
 
   _listFilterTwo(List<FilterItem> list, StateSetter setState) {
-    return ListView.builder(
-        itemBuilder: (context, index) => _cellFilterTwo(list[index], setState),
-        itemCount: list.length);
+    return RawScrollbar(
+      controller: scrollPosicioned,
+      thumbVisibility: true,
+      thickness: 5,
+      thumbColor: widget.switchColor,
+      trackVisibility: true,
+      trackColor: Colors.grey,
+      trackRadius: const Radius.circular(5),
+      radius: const Radius.circular(5),
+      child: ListView.builder(
+        controller: scrollPosicioned,
+          itemBuilder: (context, index) => _cellFilterTwo(list[index], setState),
+          itemCount: list.length),
+    );
   }
 
 // _cellFilter(FilterItem item, StateSetter setState) {
